a1918b8f5eaabca6532d3d1f5d927e515e485ec2
APACHE011 <adamm9981@gmail.com>
2020-04-16 16:52:49 -0700

    Updating JavaDocs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
 
+    /**
+     * Prints out Heyo
+     * @param args I don't rly know what this is haha
+     */
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Heyo");
